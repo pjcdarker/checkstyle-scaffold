eb8841e48751aae2352dedacb63fa054148b373c
pengjiancong <pjcdarker@gmail.com>
2021-07-28 23:39:00 +0800

    feat: add spotbugs plugin

--- src/main/java/com/pjcdarker/checkstyle/Checkstyle.java
+++ src/main/java/com/pjcdarker/checkstyle/Checkstyle.java
@@ -5,4 +5,9 @@ public class Checkstyle {
     public static void main(String[] args) {
         System.out.println("Hi Checkstyle.");
     }
+
+    public void spotbugs_check_NullPointerException() {
+        String text = null;
+        System.out.println(text.toLowerCase());
+    }
 }

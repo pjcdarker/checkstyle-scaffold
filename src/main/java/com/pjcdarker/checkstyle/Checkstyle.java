66e50746b530a96c73b356353b3375fb5542d9ca
pengjiancong <pjcdarker@gmail.com>
2021-07-29 22:47:25 +0800

    test

--- src/main/java/com/pjcdarker/checkstyle/Checkstyle.java
+++ src/main/java/com/pjcdarker/checkstyle/Checkstyle.java
@@ -3,7 +3,7 @@ package com.pjcdarker.checkstyle;
 public class Checkstyle {
 
     public static void main(String[] args) {
-        System.out.println("Hi Checkstyle v3.3.");
+        System.out.println("Hi Checkstyle v3.4.");
     }
 
     public void spotbugs_check_NullPointerException() {
